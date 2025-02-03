e437944c4b3559f663385d7a87d5d893e70693db
doppelkool <konrad.warttmann2@gmail.com>
2025-02-03 01:41:02 +0100

    Add sorting of enchantments in alphabetical order
    + add missing prefixes in plugin messages

--- src/main/java/de/doppelkool/itemforgegui/Main/PlayerMenuUtility.java
+++ src/main/java/de/doppelkool/itemforgegui/Main/PlayerMenuUtility.java
@@ -2,7 +2,6 @@ package de.doppelkool.itemforgegui.Main;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.bukkit.Bukkit;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
