2275f92a38886b13294962802a41ba7e1cc38983
doppelkool <konrad.warttmann2@gmail.com>
2025-02-23 04:57:45 +0100

    added draft for USE_TOOL PF split
    not working state

--- src/main/java/de/doppelkool/itemforgegui/Main/CustomItemManager/ForgeAction.java
+++ src/main/java/de/doppelkool/itemforgegui/Main/CustomItemManager/ForgeAction.java
@@ -10,7 +10,9 @@ package de.doppelkool.itemforgegui.Main.CustomItemManager;
  * <p>PLACE            - cannot be placed.
  * <p>EQUIP            - cannot be equipped on the player. Equipping on an armorstand is also denied
  * <p>BURN             - cannot be burned by fire or lava.
- * <p>USE_TOOL         - are tools, that cannot be used. (To be continued what exactly happens)
+ * <p>ALTER_BLOCKS     - cannot be used to alter blocks (E.g. block breaking, wood stripping, ignite TNT)
+ * <p>ALTER_ENTITIES   - cannot be used to alter entities (E.g. sheep shearing, dying a sheep)
+ * <p>NO_INTERACT      - cannot be used to interact with anything at all (E.g. Shield blocking, glow ink usage on a sign)
  * <p>REPAIR           - cannot be repaired in any way. Neither by Ingots/Membranes or similar nor by the same type of item
  * <p>UPGRADE          - cannot be upgraded from diamond to netherite
  * <p>CRAFT            - cannot be used as a crafting material
@@ -30,16 +32,65 @@ public enum ForgeAction {
 	EQUIP,      //Missing equip prevention through users Inventory and quick-equip through rightclick
 	BURN,
 
-	ALTER_BLOCKS, // for breaking blocks(BlockBreakEvent)? Axes aswell but + wood stripping(PlayerInteractEvent )?
-	//Buckets to cannot be filled or emptied? FNS only ignite TNT(BlockIgniteEvent), Fire(BlockPlaceEvent)?
+	//region ToDo
+	ALTER_BLOCKS,
+	/*
+		- Breaking Blocks (Tools, TNT explosion, bed explosion in end and nether) (BlockBreakEvent, EntityExplodeEvent)
+		- Pistons (BlockPistonExtendEvent, BlockPistonRetractEvent)
+		- Filling and emptying buckets (PlayerBucketFillEvent, PlayerBucketEmptyEvent)
+		- Wood stripping with axe (PlayerInteractEvent)
+		- Dirt/Grass to Farmland with hoe (PlayerInteractEvent)
+		- Coarse dirt to dirt with hoe (PlayerInteractEvent)
+		- ** to Path blocks with shovel (PlayerInteractEvent)
+		- Carving pumpkin with shears (PlayerInteractEvent)
+		- Removing snow (BlockBreakEvent, PlayerInteractEvent)
+		- FNS to ignite TNT/portals and "place" fire (BlockIgniteEvent, PlayerInteractEvent)
+		- Observer to dont send a signal (BlockRedstoneEvent)
+		- Saplings/Crops/Vines/ dont grow (BlockGrowEvent, StructureGrowEvent)
+		- Sculk spreading (BlockSpreadEvent)
+	 */
 
-	ALTER_ENTITIES, // hitting another entity entirely; Shears for snow golem and sheeps; dyes to dye a sheep
+	ALTER_ENTITIES,
+	/*
+		- Damaging an entity (EntityDamageEvent, EntityDamageByEntityEvent)
+		- Projectile hitting an entity (ProjectileHitEvent, EntityDamageByEntityEvent)
+		- Rod hook an entity (?, add fishing here or on NO_INTERACT?) (PlayerFishEvent)
+		- Shear a sheep (PlayerInteractEntityEvent)
+		- Shear a snow golem (PlayerInteractEntityEvent)
+		- Shear a mushroom cow (PlayerInteractEntityEvent)
+		- Using dye on sheep (PlayerInteractEntityEvent)
+		- Naming entity with a name tag (PlayerInteractEntityEvent)
+		- Feeding/breeding animals (wheat,seeds) (PlayerInteractEntityEvent)
+		- Healing animals (PlayerInteractEntityEvent)
+		- Taming wolves with bones; ocelots/cats with fish; horse with apples/sugar (PlayerInteractEntityEvent)
+		- Golden apple to cure a zombie villager (PlayerInteractEntityEvent)
+		- Trident villager to witch; pig to pigman (EntityDamageByEntityEvent)
+		- Splash/Lingering Potion to affect entity (PotionSplashEvent, LingeringPotionSplashEvent)
+		- Potion to affect player (PotionConsumeEvent, PlayerInteractEvent)
+		- Spawn egg to prevent spawning (PlayerInteractEvent)
+		- Attaching leads to entity (PlayerInteractEntityEvent)
+		- Milk a cow/mushroom cow (PlayerInteractEntityEvent)
+	 */
 
-	NO_INTERACT, // Rod fishing, Shield blocking, glow ink for signs; Carrot on a Stick to not control pigs
-
-
-
-	USE_TOOL,   //Missing use prevention of every tool
+	NO_INTERACT,
+	/*
+		- Shield blocking (PlayerInteractEvent)
+		- Throw rod (PlayerFishEvent)
+		- Spyglass usage (PlayerInteractEvent)
+		- Chests/barrels/shulker boxes/hoppers/lectern/? (InventoryOpenEvent)
+		- Using carrot or fungungs on a rod to lead pig/strider (PlayerInteractEntityEvent)
+		- Anvil, Grindstone, Loom, Cartography table, smithing table, enchanting table (InventoryOpenEvent)
+		- Open Doors, trapdoors, fence gates (PlayerInteractEvent)
+		- Pressing buttons, pulling levers (PlayerInteractEvent)
+		- Ringing bells (PlayerInteractEvent)
+		- Glow ink on sign (?, re-edit a sign?) (PlayerEditSignEvent, SignChangeEvent)
+		- Ride minecart/boat (VehicleEnterEvent)
+		- Map creation(right click) (PlayerInteractEvent)
+		- Bed to sleep and set spawnpoint (PlayerBedEnterEvent)
+		- Lead on a fence post (PlayerInteractEvent)
+		- Jukebox/note block sounds (PlayerInteractEvent)
+	 */
+	//endregion
 
 	REPAIR,
 	UPGRADE,
