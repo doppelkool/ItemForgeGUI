0deb5357ba47a0ea3ba968de11d7e1869500a19b
Konrad <56027130+doppelkool@users.noreply.github.com>
2026-07-19 03:03:40 +0200

    Update to 26.1.2
    * Added new itemFlag "HIDE_STORED_ENCHANT/S/MENTS"
    * Fix a bug in displaying the api item after menu updates
     (#19)

--- src/main/java/de/doppelkool/itemforgegui/Main/CustomItemManager/Flags/ItemFlagManager.java
+++ src/main/java/de/doppelkool/itemforgegui/Main/CustomItemManager/Flags/ItemFlagManager.java
@@ -21,14 +21,24 @@ public class ItemFlagManager {
 	protected final Map<Integer, Pair<ItemFlag, ItemStack>> slotToFlag = new HashMap<>();
 
 	public ItemFlagManager() {
+		ItemFlag storedEnchantmentsFlag;
+
+		// Spigots experimental HIDE_STORED_ENCHANTMENTS is equivalent to Papers stable HIDE_STORED_ENCHANTS. Since Papers is not known to Spigot, we use a workaround to load it onDemand.
+		if (isPaper()) {
+			storedEnchantmentsFlag = ItemFlag.valueOf("HIDE_STORED_ENCHANTS");
+		} else {
+			storedEnchantmentsFlag = ItemFlag.HIDE_STORED_ENCHANTMENTS;
+		}
+
 		slotToFlag.put(11, new Pair<>(ItemFlag.HIDE_ENCHANTS, MinecraftItemFlagItems.hideEnchantments));
-		slotToFlag.put(12, new Pair<>(ItemFlag.HIDE_ATTRIBUTES, MinecraftItemFlagItems.hideAttributes));
-		slotToFlag.put(13, new Pair<>(ItemFlag.HIDE_UNBREAKABLE, MinecraftItemFlagItems.hideUnbreakable));
-		slotToFlag.put(14, new Pair<>(ItemFlag.HIDE_DESTROYS, MinecraftItemFlagItems.hideDestroys));
-		slotToFlag.put(15, new Pair<>(ItemFlag.HIDE_PLACED_ON, MinecraftItemFlagItems.hidePlacedOn));
-		slotToFlag.put(21, new Pair<>(ItemFlag.HIDE_ADDITIONAL_TOOLTIP, MinecraftItemFlagItems.hideAdditionalToolTip));
-		slotToFlag.put(22, new Pair<>(ItemFlag.HIDE_DYE, MinecraftItemFlagItems.hideDye));
-		slotToFlag.put(23, new Pair<>(ItemFlag.HIDE_ARMOR_TRIM, MinecraftItemFlagItems.hideArmorTrim));
+		slotToFlag.put(12, new Pair<>(storedEnchantmentsFlag, MinecraftItemFlagItems.hideStoredEnchantments));
+		slotToFlag.put(13, new Pair<>(ItemFlag.HIDE_ATTRIBUTES, MinecraftItemFlagItems.hideAttributes));
+		slotToFlag.put(14, new Pair<>(ItemFlag.HIDE_UNBREAKABLE, MinecraftItemFlagItems.hideUnbreakable));
+		slotToFlag.put(15, new Pair<>(ItemFlag.HIDE_DESTROYS, MinecraftItemFlagItems.hideDestroys));
+		slotToFlag.put(21, new Pair<>(ItemFlag.HIDE_PLACED_ON, MinecraftItemFlagItems.hidePlacedOn));
+		slotToFlag.put(22, new Pair<>(ItemFlag.HIDE_ADDITIONAL_TOOLTIP, MinecraftItemFlagItems.hideAdditionalToolTip));
+		slotToFlag.put(23, new Pair<>(ItemFlag.HIDE_DYE, MinecraftItemFlagItems.hideDye));
+		slotToFlag.put(24, new Pair<>(ItemFlag.HIDE_ARMOR_TRIM, MinecraftItemFlagItems.hideArmorTrim));
 	}
 
 	public static ItemFlagManager getInstance() {
@@ -36,5 +46,14 @@ public class ItemFlagManager {
 			instance = new ItemFlagManager();
 		}
 		return instance;
+	}
+
+	private static boolean isPaper() {
+		try {
+			Class.forName("io.papermc.paper.configuration.Configuration");
+			return true;
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
 	}
 }
